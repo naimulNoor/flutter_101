62930225dc0b0792fa21d4e54ba08f09934c44b1
naimulNoor <naimul.roni@outlook.com>
2020-07-15 20:29:32 +0600

    init

--- lib/main.dart
+++ lib/main.dart
@@ -1,56 +1,69 @@
+import 'dart:math';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'PlaceDetails.dart';
+
 
 void main(){
-  runApp(PleaceDetails());
+  runApp(MyFlutterApp());
 }
 
 class MyFlutterApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.blue
+          primarySwatch: Colors.blue
       ),
-      //first page opining
-      home: SafeArea(
-        child: Scaffold(//scafflod is one kind of matarial app
-          appBar: AppBar(
-            title: Text("My First Flutter App"),
-          ),
-          body: Padding(
-            padding:  EdgeInsets.symmetric(horizontal:0.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: <Widget>[
-                Image.network('https://picsum.photos/200/300?grayscale',
-                height: 200.0,
-                width: 200.0,),
-                Text('Naimul Hassan Noor',style: TextStyle(fontSize: 20.0,color: Colors.redAccent,),
-                textAlign: TextAlign.center,
-                ),
-                Text('Software Engineer',style: TextStyle(fontSize: 20.0,color: Colors.redAccent,),
-                textAlign: TextAlign.center,
-                ),
-                Text('Works at Infolink LTD',style: TextStyle(fontSize: 20.0,color: Colors.redAccent,),
-                textAlign: TextAlign.center,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: <Widget>[
-                    Padding(
-                        padding: EdgeInsets.all(20.0),
-                        child: Icon(Icons.call,color: Colors.blue,)),
-                    Text('call me',style: TextStyle(fontSize: 20.0,color: Colors.redAccent,), textAlign: TextAlign.center,),
-                  ],
-                )
-              ],
-            ),
-          ),
+      home: DiceGame(),
+
+    );
+  }
+}
+
+class DiceGame extends StatefulWidget {
+  @override
+  _DiceGameState createState() => _DiceGameState();
+}
+
+class _DiceGameState extends State<DiceGame> {
+  final random = Random.secure() ;
+  var index=0;
+  final dices =[
+    'assest/dice/d4.jpeg',
+    'assest/dice/Dice-1.png',
+    'assest/dice/d4.jpeg',
+    'assest/dice/Dice-2E.png',
+    'assest/dice/dice-3.png',
+    'assest/dice/dice-6.png',
+    'assest/dice/dice-5.jpg'
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Dice Game"),
+      ),
+      body: Center(
+
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+              Image.asset(dices[index],width: 72,height: 72,fit: BoxFit.cover,),
+              RaisedButton(
+                child: Text('Roll Dice'),
+                onPressed: (){
+                  setState(() {
+                   index =random.nextInt(6);
+                  });
+
+
+                },
+              )
+          ],
         ),
-      )
+      ),
     );
   }
 }
